d8ad1e38dbf256bf079a8500359e9ef673cac242
Potewo <potewo@protonmail.com>
2022-11-23 21:55:46 +0900

    add minimum functions

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
+  import { FormGroup, Label, Input, Icon, Button } from "sveltestrap";
+  let text: string = "test";
 </script>
 
 <main>
-
+  <FormGroup>
+    <Label for="exampleText">Text Area</Label>
+    <Input type="textarea" name="text" id="exampleText" bind:value={text} />
+  </FormGroup>
+  <div class="d-grid gap-3">
+    <div class="output-textbox border round">
+      <sub>{text}</sub>
+      <!--
+      <Button>
+        <Icon name="clipboard" />
+      </Button>
+      -->
+    </div>
+    <div class="output-textbox border round">
+      <sup>{text}</sup>
+      <!--
+      <Button>
+        <Icon name="clipboard" />
+      </Button>
+      -->
+    </div>
+  </div>
 </main>
